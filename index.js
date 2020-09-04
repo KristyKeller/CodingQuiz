0cb6d67f7d3038995b56da1741a4f27b0ae7eab3
Kristy Keller <keller.kristy@outlook.com>
2020-09-04 10:26:14 -0400

    outline added

---
+++ index.js
@@ -0,0 +1,20 @@
+// var timer = 60;
+// var questions = [
+//     {
+//         questionTitle: "What is"
+//         choice1: "",
+//         choice2: "",
+//         answer: ""
+//     },
+// ];
+// //Array of objects to store questions using a string comparison (reference gandalf activity)
+
+// if(choice3Clicked) {
+//     if(questions[currentQuestion]){
+//         [choice3Clicked.id] === questions
+// }
+// else 
+// // Correct answer, you lose x amount of seconds 
+// // Wrong answer, you lose x amount of seconds, will need sections
+// currentQuestion++
+// displayQuestion ();
