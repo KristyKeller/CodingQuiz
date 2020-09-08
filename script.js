690680c994178669885f2c2b8f6df0cb721386cf
Kristy Keller <keller.kristy@outlook.com>
2020-09-07 23:39:24 -0400

    javascript question updates

--- script.js
+++ script.js
@@ -1,33 +1,64 @@
 // Prompt user to start quiz
 // Once they click the start button, it will display the first quiz question
-const start = document.getElementById("start");
-const quiz = document.getElementById("quiz"); 
-const question = document.getElementById("question");
-const 
+
+//select all elements for quiz
+var start = document.getElementById("start");
+var quiz = document.getElementById("quiz"); 
+var question = document.getElementById("question");
+var choice1 = document.getElementById("1");
+var choice2 = document.getElementById("2");
+var choice3 = document.getElementById("3");
+var choice4 = document.getElementById("4");
+var scoreDiv = document.getElementById("score");
 var timer = 60;
-var questions = ["Question 1", "Question 2", "Question 3", "Question 4", "Question 5", "Question 6"]; 
-// Question render
-let lastQuestionIndex = questions.length -1;
-let runningQuestionIndex = 0;
-function renderQuestion (){
-    let q= questions[runningQuestionIndex];
+ 
+// Variables for questions
+let questions = [];
+var lastQuestion = questions.length -1;
+let runningQuestion = 0;
+
+// Render questions
+function renderQuestion(){
+    let q = questions.[runningQuestion];
     question.innerHTML = "<p>" + q.question + "</p>"; 
     choice1.innerHTML = q.choice1; 
     choice2.innerHTML = q.choice2;
     choice3.innerHTML = q.choice3;
-    choice4.innerHTML = q.choice4;
-} 
+    choice4.innerHTML = q.choice4; 
+}
+
+// Render timer
+var questionRight
+var questionWrong
+
+function renderTimer(){
+    if (timer ) //confused how to get timer to respond to right or wrong responses 
+}
+//Hide start button and render questions based on clicks
+start.style.display = "none";
+renderQuestion();
+quiz.style.display = "block";
+start.addEventListener("click",startQuiz
+//Start quiz
+function startQuiz(){
+    start.style.display = "none";
+    renderQuestion();
+    quiz.style.display = "block";
+    renderTimer();
+    timer= setInterval(renderCounter ,1000);
+}
+
 // Question choices and correct answers
     let questions = [
     {
-        Question1: "How to write an IF statement for executing some code if 'i' is NOT equal to 5?",
+        question: "How to write an IF statement for executing some code if 'i' is NOT equal to 5?",
         choice1: "if i =! 5 then",
         choice2: "if (i <> 5)",
         choice3: "if i <> 5",
         choice4: "if (i != 5)",
         correct: "4"
     },
-    {   Question2: "Question 2: How does a WHILE loop start?",
+    {   question: "Question 2: How does a WHILE loop start?",
         choice1: "while (i <= 10;i++)",
         choice2: "while i = 1 to 10",
         choice3: "while (i <= 10)",
@@ -35,7 +66,7 @@ function renderQuestion (){
         correct: "3"
 
     },
-    {   Question3: "Question 3: How does a FOR loop start?",
+    {   question: "Question 3: How does a FOR loop start?",
         choice1: "for (i = 0; i <= 5; i++)",
         choice2: "for (i <= 5; i++)",
         choice3: "for (i = 0; i <= 5)",
@@ -43,7 +74,7 @@ function renderQuestion (){
         correct: "1" 
 
     },
-    {   Question4: "Question 4: How do you write 'Hello World' in an alert box?",
+    {   question: "Question 4: How do you write 'Hello World' in an alert box?",
         choice1:  "alert("Hello World");",
         choice2:  "alertBox("Hello World");",
         choice3:  "msgBox("Hello World");",
@@ -51,7 +82,7 @@ function renderQuestion (){
         correct:  "1"
 
     },
-    {   Question5: "Question 5: How do you create a function in JavaScript?",
+    {   question: "Question 5: How do you create a function in JavaScript?",
         choice1: "function = myFunction()",
         choice2: "function myFunction()",
         choice3: "function:MyFunction()",
@@ -59,7 +90,7 @@ function renderQuestion (){
         correct: "2"
 
     },
-    {   Question6: 
+    {   question: "Question 6:  How to write an IF statement in JavaScript?"
         choice1: "if i==5 then",
         choice2: "if (i==5)",
         choice3: "if i==5 then",
@@ -68,14 +99,3 @@ function renderQuestion (){
 
     }
 ];
-// //Array of objects to store questions using a string comparison (reference gandalf activity)
-
-// if(choice3Clicked) {
-//     if(questions[currentQuestion]){
-//         [choice3Clicked.id] === questions
-// }
-// else 
-// // Correct answer, you lose x amount of seconds 
-// // Wrong answer, you lose x amount of seconds, will need sections
-// currentQuestion++
-// displayQuestion ();
